8effc7d07981c287614dd9957b7b12f9fb966082
kocio07 <mlody.synowiec@gmail.com>
2026-08-24 14:00:24 +0200

    added live kamera frames

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const video = document.getElementById('ekran');
+const canvas = document.getElementById('gra');
+const ctx = canvas.getContext('2d');
 
 navigator.mediaDevices.getUserMedia({ video: true })
   .then(stream => {
@@ -6,4 +8,15 @@ navigator.mediaDevices.getUserMedia({ video: true })
   })
   .catch(err => console.error('Brak dostępu do kamery:', err));
 
-  
+  function loop() {
+    if (video.readyState >= 2) {
+        ctx.save();
+        ctx.translate(canvas.width, 0);
+        ctx.scale(-1, 1);
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        ctx.restore();
+    }
+    requestAnimationFrame(loop);
+  }
+  loop();
+ 
