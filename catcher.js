f7ed7fc79a488b845ef42bf85c220bc581bf7345
kocio07 <mlody.synowiec@gmail.com>
2026-08-26 19:41:22 +0200

    created asket place holder

--- catcher.js
+++ catcher.js
@@ -1,22 +1,48 @@
 import { HandLandmarker, FilesetResolver } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.14";
 
-const HAND_CONNECTIONS = [
-  [0,1],[1,2],[2,3],[3,4],           // kciuk
-  [0,5],[5,6],[6,7],[7,8],           // wskazujący
-  [5,9],[9,10],[10,11],[11,12],      // środkowy
-  [9,13],[13,14],[14,15],[15,16],    // serdeczny
-  [13,17],[17,18],[18,19],[19,20],   // mały palec
-  [0,17]                             // nadgarstek
-];
 
-const palmCenter = points[9];
+
 
 const video = document.getElementById('ekran1');
-
 const canvas = document.getElementById('gra1');
 const ctx = canvas.getContext('2d');
 
-const drawCanvas = document.getElementById('plotno1');
-const drawCtx = drawCanvas.getContext('2d');
+let hands = [];
+let handLandmarker = null;
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+window.addEventListener('resize', resizeCanvas);
 
 
+async function initHandLandmarker() {
+  const vision = await FilesetResolver.forVisionTasks(
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.14/wasm"
+  );
+  handLandmarker = await HandLandmarker.createFromOptions(vision, {
+    baseOptions: {
+      modelAssetPath:  "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task",
+      delegate: "GPU"
+    },
+    runningMode: "VIDEO",
+    numHands: 1
+  });
+}
+
+async function initCamera() {
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+  video.srcObject = stream;
+  return new Promise(resolve => {
+    video.onloadeddata = () => resolve();
+  });
+}
+
+function drawBasket(x, y) {
+  ctx.font = '70px serif';
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'middle';
+  ctx.fillText('B', x, y);
+
+}
