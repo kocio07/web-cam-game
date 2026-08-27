4b6e251bfe49dc319ab5f0c9159f81bfdf6eff33
kocio07 <mlody.synowiec@gmail.com>
2026-08-27 10:25:57 +0200

    added utton listener

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 document.getElementById('paintb').addEventListener('click', () => {
     window.location.href = 'index.html';
 });
+
+document.getElementById('catcherb').addEventListener('click', () => {
+    window.location.href = 'catcher.html';
+})
